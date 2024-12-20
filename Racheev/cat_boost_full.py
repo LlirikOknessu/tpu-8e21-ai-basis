0c18003b08666b9fe06773ecb8a78e51c1b2bd96
PavelORT <rpongg@gmail.com>
2024-12-21 00:08:54 +0700

    Cat boosting

--- Racheev/cat_boost_full.py
+++ Racheev/cat_boost_full.py
@@ -1,20 +1,75 @@
-'''from catboost import CatBoostRegressor
+import pandas as pd
+import argparse
+from pathlib import Path
+import yaml
+import numpy as np
+from joblib import dump, load
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import GridSearchCV
+import random
+from catboost import CatBoostRegressor
+from catboost import Pool, cv
 
-model = CatBoostRegressor(iterations=3000,
-                          early_stopping_rounds=100,
-                          grow_policy='Depthwise',
-                          depth=8,
-                          loss_function=RMSLE(),
-                          cat_features=CAT_COLS,
-                          random_state=RS,
-                          l2_leaf_reg=1,
-                          learning_rate=0.03,
-                          verbose=10,
-                          eval_metric=RMSLE_val())
+CATBOOST_MODELS_MAPPER = {'CatBoostRegressor': CatBoostRegressor}
 
-params = {'l2_leaf_reg':[1,4,8],
-          'learning_rate': [0.03,0.5,0.1]
-          'depth':[6,8,10]
-         }
-grid_search_res = model.grid_search(params, full_features['items'][FTS_COLS], full_features['items'].target, train_size=0.8)
-'''
+# Set the best parameters that you get on training stage for all used models
+CATBOOST_MODELS_BEST_PARAMETERS = {
+    'CatBoostRegressor': {'depth': 10, 'learning_rate': 0.05, 'iterations': 100}}
+
+def parser_args_for_sac():
+    parser = argparse.ArgumentParser(description='Paths parser')
+    parser.add_argument('--input_dir', '-id', type=str, default='data/prepared/',
+                        required=False, help='path to input data directory')
+    parser.add_argument('--output_dir', '-od', type=str, default='data/models/',
+                        required=False, help='path to save prepared data')
+    parser.add_argument('--model_name', '-mn', type=str, default='LR', required=False,
+                        help='file with dvc stage params')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parser_args_for_sac()
+
+    input_dir = Path(args.input_dir)
+    output_dir = Path(args.output_dir)
+
+    output_dir.mkdir(exist_ok=True, parents=True)
+    #output_model_path = output_dir / (args.model_name + '_prod.jpg') # for visualisation
+    output_model_joblib_path = output_dir / (args.model_name + '_prod.joblib')
+
+    X_train_name = input_dir / 'X_full.csv'
+    y_train_name = input_dir / 'y_full.csv'
+
+    X_train = pd.read_csv(X_train_name)
+    y_train = pd.read_csv(y_train_name)
+    y_train_cols = y_train.columns
+
+    best_params = CATBOOST_MODELS_BEST_PARAMETERS.get(args.model_name)
+    reg = CATBOOST_MODELS_MAPPER.get(args.model_name)(**best_params)
+    reg = reg.fit(X_train, y_train, verbose=False)
+
+
+    predicted_values = np.squeeze(reg.predict(X_train_name))
+
+    print(reg.score(X_train_name, y_train_name))
+    print(reg.get_params)
+
+    print("Model MAE: ", mean_absolute_error(y_train_name, predicted_values))
+
+    feature_importance = reg.get_feature_importance()
+    feature_names = X_train.columns
+
+    # Display feature importance
+    for name, importance in zip(feature_names, feature_importance):
+        print(f"Feature: {name}, Importance: {importance:.2f}")
+
+    '''
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=feature_importance, y=feature_names, color = 'b')
+    plt.title('Feature Importance')
+    plt.xlabel('Importance')
+    plt.ylabel('Features')
+    plt.show()
+    '''
+    dump(reg, output_model_joblib_path)
